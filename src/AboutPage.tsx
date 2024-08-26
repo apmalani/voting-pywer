fece759c32c981999e591d7c5b73014988405d8c
arun <apmalani@uci.edu>
2024-08-26 16:38:05 -0700

    changes to the about page to match ui changes

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -1,48 +1,38 @@
 import React from 'react';
 import HeaderComponent from './HeaderComponent';
+import './AboutPage.css';
 
 const AboutPage = () => {
     return (
         <div className='AboutPage'>
-            <HeaderComponent></HeaderComponent>
-            <h3 style={{marginLeft: '0px', marginTop: '10px'}}>About</h3>
-
-            <div style={{marginLeft: '0px'}}>
+            <HeaderComponent />
+            <div className="about-content">
+                <h3>About Voting Pywer</h3>
                 <p>
-                    Voting Pywer was developed to improve upon <a href="http://powerslave.utu.fi/">Powerslave Mark II</a>, a similar tool developed by the University of Turku.
-                    Mark II was last updated in 2016, and was moved to a virtual server in 2021.
-                    <br/>
-                    It features a much cleaner user interface, as well as the ability to calculate more than 2000 individual votes and 10 unique parties simultaneously.
-                    <br/>
-                    Voting Pywer currently calculates two power indices, however more are soon to be implemented. The two currently implemented are the Banzhaf and Shapley-Shubik power indices,
-                    which are both commonly used in voting theory.
-                    <br/>
-                    For other indices, as well as more in-depth explanations of the indices and the math behind them, please visit Mark II's site.
-                    <br/>
+                    Voting Pywer was developed to enhance the functionality of <a href="http://powerslave.utu.fi/">Powerslave Mark II</a>, a similar tool created by the University of Turku. 
+                    Mark II, last updated in 2016 and moved to a virtual server in 2021, has been improved with a cleaner user interface and the ability to handle more than 2000 individual votes and up to 10 unique parties simultaneously.
+                </p>
+                <p>
+                    Currently, Voting Pywer calculates two power indices: the Banzhaf and Shapley-Shubik power indices, both widely used in voting theory. Future updates will include additional indices and features.
                 </p>
                 <h5>Indices</h5>
                 <p>
-                    The Banzhaf voting power index is calculated for a voter "v" by dividing the number of times "v" is critical by
-                    the total number of times all voters are critical.
-                    <br/>
-                    The Shapley-Shubik voting power index is calculated for a voter "v" by dividing the number of times "v" is pivotal
-                    by the total number of sequential coalitions.
-                    <br/>
-                    The words "critical" and "pivotal" are almost interchangeable. Here, voter "v" is "critical" for a winning coalition if,
-                    without "v", the coalition is losing. 
-                    <br/>
-                    Similarly, voter "v" is pivotal if joining a sequential coalition (a coaliton in which each party joins
-                    as the sequence is written out) causes the coalition to go from losing to winning. 
-                    <br/>
-                    Essentially, critical/pivotal parties cause losing coalitions to become winning coalitions when they join, and vice versa when they leave.
-                    <br/>
-                    <br/>
-                    Note: As mentioned, Voting Pywer currently allows for 10 distinct parties. This number will hopefully go up if I am able to get the site hosted on my university's servers.
-                    <br/>
-                    At the max of 10 parties, calculations may take a few seconds, so please be patient.
-                    <br/>
-                    Many thanks to the developers of Powerslave and to Professor Kaminski.
-                
+                    The <strong>Banzhaf voting power index</strong> for a voter "v" is calculated by dividing the number of times "v" is critical by the total number of times all voters are critical.
+                </p>
+                <p>
+                    The <strong>Shapley-Shubik voting power index</strong> for a voter "v" is determined by dividing the number of times "v" is pivotal by the total number of sequential coalitions.
+                </p>
+                <p>
+                    In this context, "critical" and "pivotal" are used interchangeably. A voter "v" is "critical" in a winning coalition if removing "v" makes the coalition losing. Similarly, "v" is pivotal if joining a sequential coalition (where each party joins in the sequence specified) changes the coalition from losing to winning.
+                </p>
+                <p>
+                    In essence, critical or pivotal parties shift coalitions from losing to winning and vice versa when they join or leave.
+                </p>
+                <p>
+                    Note: Voting Pywer currently supports up to 10 distinct parties. This number might increase if the site gets hosted on university servers. For the maximum of 10 parties, calculations may take a few seconds.
+                </p>
+                <p>
+                    Special thanks to the developers of Powerslave and to Professor Kaminski for their contributions.
                 </p>
             </div>
         </div>
